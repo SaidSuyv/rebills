c9ac10e26754bc051df4e539f8a0e261144fa18d
SaidSuyv <sirsuyvi@gmail.com>
2025-06-07 12:17:18 -0500

    update: layout

--- src/layouts/dashboard.layout.jsx
+++ src/layouts/dashboard.layout.jsx
@@ -1,11 +1,12 @@
 import NavComponent from '@/components/main/nav.component'
+import { Outlet } from 'react-router-dom';
 
-export default function DashbardLayout ({ children })
+export default function DashbardLayout ()
 {
     return (
-        <main>
+        <main className='h-full grid grid-rows-12 md:grid-rows-1 md:grid-cols-12'>
             <NavComponent />
-            { children }
+            <Outlet />
         </main>
     );
 }
